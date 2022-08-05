0ee75a836f99289b045b804ed34c03ab4e65797e
Emerson Millard <emillard.code@gmail.com>
2022-08-05 12:00:00 -0700

    Added functionality.

---
+++ src/main/java/predefined_interfaces/function/FunctionApplication.java
@@ -0,0 +1,26 @@
+package predefined_interfaces.function;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class FunctionApplication {
+
+    public static void main( String[] args ) {
+
+        // Basic Function implementation
+
+        Function<String, Integer> function = s -> s.length();
+
+        System.out.println(function.apply("Hello World!"));
+
+        // Using Function with Java 8 Streams
+
+        List<String> list = Arrays.asList("One", "Two", "Three", "Four");
+
+        list.stream().map(function).forEach(i -> System.out.println(i));
+
+    }
+
+}
